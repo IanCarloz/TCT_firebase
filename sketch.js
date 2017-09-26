44317750061c0eeb083485aaa376c2eda859d9af
IanCarloz <iancarloz27g@gmail.com>
2017-09-26 13:57:56 -0500

    first time scorellist showed :)

--- sketch.js
+++ sketch.js
@@ -40,14 +40,16 @@ function gotData(data) {
   var scores = data.val();
   var keys = Object.keys(scores);
 
-  console.log(keys);
+  // console.log(keys);
 
   for (var i = 0; i < keys.length; i++) {
     var k = keys[i];
     var initials = scores[k].initials;
     var score = scores[k].score;
     // console.log('initials: '+initials+', score: '+ score);
-    console.log(initials, score);
+    // console.log(initials, score);
+    var li = createElement('li',  initials+": "+score);
+    li.parent('scorelist'); 
   }
 
 }
