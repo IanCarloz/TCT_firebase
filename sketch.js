df42c121ec51ac92d2da6fd76d1b309a9473d134
IanCarloz <iancarloz27g@gmail.com>
2017-09-26 13:49:28 -0500

    Parsing the initials and scores.

--- sketch.js
+++ sketch.js
@@ -39,7 +39,16 @@ function gotData(data) {
   // console.log(data.val());
   var scores = data.val();
   var keys = Object.keys(scores);
+
   console.log(keys);
+
+  for (var i = 0; i < keys.length; i++) {
+    var k = keys[i];
+    var initials = scores[k].initials;
+    var score = scores[k].score;
+    // console.log('initials: '+initials+', score: '+ score);
+    console.log(initials, score);
+  }
 
 }
 
